e1d01e6099429418a32434bc897f66c7b4917e92
Harald Hoyer <harald@hoyer.xyz>
2024-12-05 11:45:37 +0100

    iii
    Signed-off-by: Harald Hoyer <harald@hoyer.xyz>

--- flake.nix
+++ flake.nix
@@ -1,8 +1,37 @@
 {
-  inputs.nixify.url = "github:rvolosatovs/nixify";
+  inputs = {
+    nixify.url = "github:rvolosatovs/nixify";
+    nixsgx-flake.url = "github:matter-labs/nixsgx";
+    nixpkgs.follows = "nixsgx-flake/nixpkgs";
+  };
 
-  outputs = { nixify, ... }:
+  outputs = { nixify, nixsgx-flake, ... }:
     nixify.lib.rust.mkFlake {
       src = ./.;
+
+      overlays = [
+        nixsgx-flake.overlays.default
+      ];
+
+      withDevShells =
+        { devShells
+        , pkgs
+        , ...
+        }:
+        nixify.lib.extendDerivations
+          {
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              rustPlatform.bindgenHook
+            ];
+
+            buildInputs = with pkgs; [
+              openssl
+              nixsgx.sgx-sdk
+              nixsgx.sgx-dcap
+              nixsgx.sgx-dcap.libtdx_attest
+            ];
+          }
+          devShells;
     };
 }
